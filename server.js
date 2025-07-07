ffab0392d5b6dfcbb6029ba87871b80e52d57e79
Nomfundo Faith Khoza <nomfundofaithkhoza@gmail.com>
2025-07-07 16:13:02 +0200

    commit changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+console.log("My custom message:", process.env.CUSTOM_MESSAGE);
 const express = require ('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 
 const app =express();
-const PORT = Process.env.PORT ||3000;
+const PORT = process.env.PORT ||3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
